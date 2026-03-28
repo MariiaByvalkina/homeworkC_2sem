2d9f0282d42beb32ac1b1c6e48c672155a3e9143
Maria Byvalkina <masabyvalkina67715@gmail.com>
2026-03-29 00:40:49 +0300

    fix clang-tidy mistakes in header

--- src/graphStates/graphStates.h
+++ src/graphStates/graphStates.h
@@ -1,11 +1,11 @@
 #pragma once
 
 typedef struct Graph Graph;
-typedef struct priorityQueue priorityQueue;
+typedef struct PriorityQueue PriorityQueue;
 
 Graph* createGraph(int n);
 void addEdge(Graph* graph, int start, int finish, int dist);
 void freeGraph(Graph* graph);
-priorityQueue* createQueue(int size);
+PriorityQueue* createQueue(int size);
 char* modifiedDijkstra(Graph* graph, int* capitals, int n, int k);
 void readFile(const char* filename);
