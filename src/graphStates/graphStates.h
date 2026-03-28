10be32e84ab9b5c7bc728abb330348fe0ebb158e
Maria Byvalkina <masabyvalkina67715@gmail.com>
2026-03-29 00:49:10 +0300

    fix mistakes in header again

--- src/graphStates/graphStates.h
+++ src/graphStates/graphStates.h
@@ -6,6 +6,6 @@ typedef struct PriorityQueue PriorityQueue;
 Graph* createGraph(int n);
 void addEdge(Graph* graph, int start, int finish, int dist);
 void freeGraph(Graph* graph);
-PriorityQueue* createQueue(int size);
+PriorityQueue* createQueue(int capacity);
 char* modifiedDijkstra(Graph* graph, int* capitals, int n, int k);
 void readFile(const char* filename);
